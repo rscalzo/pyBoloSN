1fb9df7dfd79fe7ce13db4d8c799fffce5d11d60
Richard Scalzo <richard.scalzo@sydney.edu.au>
2018-01-19 10:57:59 +1100

    Added new helpful command-line flags

--- run_S12_mcmc.py
+++ run_S12_mcmc.py
@@ -47,12 +47,22 @@ def main():
                         help="Distance modulus offset to apply")
     parser.add_argument('--mu_err', type=float, default=0.001,
                         help="Distance modulus uncertainty")
+    parser.add_argument('--aNi', type=float, default=0.2,
+                        help="56Ni mixing scale (in mass units)")
+    parser.add_argument('--aNi_err', type=float, default=0.1,
+                        help="56Ni mixing scale uncertainty")
+    parser.add_argument('--holefe', action='store_true', default=False,
+                        help="Turn on 56Ni hole due to stable Fe production?")
     parser.add_argument('--uvcor_lo', type=float, default=0.0,
                         help="Lower bound on UV flux correction")
     parser.add_argument('--uvcor_hi', type=float, default=0.1,
                         help="Upper bound on UV flux correction")
     parser.add_argument('--qtabfname', default='qint_exp.pkl',
                         help="Pickled tabulated values of q form factor")
+    parser.add_argument('--nsamples', type=int, default=100000,
+                        help="Number of MCMC samples to draw")
+    parser.add_argument('--ensemblesamp', action='store_true', default=False,
+                        help="Use emcee EnsembleSampler instead of PTSampler")
     args = parser.parse_args()
 
     sn_name = re.search("\/(SN[F12].*|MPA.*|LSQ.*|PTF.*)\/",
@@ -77,6 +87,7 @@ def main():
                 trise=args.trise, trise_err=args.trise_err,
                 alpha=args.alpha, alpha_err=args.alpha_err,
                 uvcor_lo=args.uvcor_lo, uvcor_hi=args.uvcor_hi,
+                aNi=args.aNi, aNi_err=args.aNi_err, holefe=args.holefe,
                 qtabpkl=args.qtabfname, sn_name=sn_name)
     else:
         mcmc = Scalzo12_MCMC_EBMV(
@@ -85,9 +96,17 @@ def main():
                 trise=args.trise, trise_err=args.trise_err,
                 alpha=args.alpha, alpha_err=args.alpha_err,
                 uvcor_lo=args.uvcor_lo, uvcor_hi=args.uvcor_hi,
+                aNi=args.aNi, aNi_err=args.aNi_err, holefe=args.holefe,
                 qtabpkl=args.qtabfname, sn_name=sn_name)
-    # mcmc.run_mcmc(sampler='PTSampler')
-    mcmc.run_mcmc(sampler='EnsembleSampler')
+    # Include a switch to run EnsembleSampler
+    if args.ensemblesamp:
+        print "WARNING: Using emcee.EnsembleSampler for this run"
+        print "Recommended for testing only.  Sampling will run faster,"
+        print "but this problem is multi-modal so watch out!"
+        mcmc.run_mcmc(sampler='EnsembleSampler')
+    else:
+        print "Using emcee.PTSampler for this run"
+        mcmc.run_mcmc(sampler='PTSampler')
     mcmc.show_results()
 
 # ----------------------------------------------------------------------------
